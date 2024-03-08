b78094e9dd23877fc1aa658a61a15708f6da0bea
jmarusak <jmarusak@users.noreply.github.com>
2024-03-08 17:57:42 -0500

    auxiliary constructor for while numbers

--- src/main/scala/com/maly/rational/Rational.scala
+++ src/main/scala/com/maly/rational/Rational.scala
@@ -1,3 +1,17 @@
 class Rational(n: Int, d: Int) {
+  require(d != 0, "rational numbers may not have a zero in the denominator")
+
+  val numer: Int = n
+  val denom: Int = d
+
+  // auxiliary construction for whole numbers
+  def this(n: Int) = this(n, 1)
+
   override def toString = s"$n/$d"
+
+  def add(that: Rational): Rational =
+    new Rational(
+      this.numer * that.denom + that.numer * this.denom,
+      this.denom * that.denom
+    )
 }
