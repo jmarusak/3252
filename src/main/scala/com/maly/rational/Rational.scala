4df6025a9516e07b0cbf8e526e88b5b4a4ba4963
jmarusak <jmarusak@users.noreply.github.com>
2024-03-10 14:38:42 -0400

    adding + - * / methods

--- src/main/scala/com/maly/rational/Rational.scala
+++ src/main/scala/com/maly/rational/Rational.scala
@@ -1,17 +1,38 @@
 class Rational(n: Int, d: Int) {
   require(d != 0, "rational numbers may not have a zero in the denominator")
 
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
 
   // auxiliary construction for whole numbers
   def this(n: Int) = this(n, 1)
 
-  override def toString = s"$n/$d"
-
-  def add(that: Rational): Rational =
+  def + (that: Rational): Rational =
     new Rational(
       this.numer * that.denom + that.numer * this.denom,
       this.denom * that.denom
     )
+
+  def - (that: Rational): Rational =
+    new Rational(
+      this.numer * that.denom - that.numer * this.denom,
+      this.denom * that.denom
+    )
+
+  def * (that: Rational): Rational =
+    new Rational(
+      this.numer * that.numer,
+      this.denom * that.denom
+    )
+
+  def / (that: Rational): Rational =
+    new Rational(
+      this.numer * that.denom,
+      this.denom * that.numer
+    )
+
+  override def toString = s"$numer/$denom"
+
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
