ed15c6b9c7f7461c7594505c2984edc6fd14cfbe
jmarusak <jmarusak@users.noreply.github.com>
2024-06-11 00:31:45 +0000

    spark IoT analysis

--- src/main/scala/SparkIoTApp.scala
+++ src/main/scala/SparkIoTApp.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, desc, avg}
 
 object SparkIoTApp {
 
@@ -11,6 +12,21 @@ object SparkIoTApp {
       .getOrCreate()
 
     val df = spark.read.format("json").load("data/iot_devices.json")
-    df.show()
+
+    val deviceCount = df.where(col("device_name").contains("sensor-pad") and col("cca3") === "POL").count
+    println(s"Answer 2.1: There is $deviceCount reported from Poland")
+
+    val colorCount = df.groupBy(col("lcd")).count.count
+    println(s"Answer 2.2: There is $colorCount distinct colors in the dataset")
+
+    val df2 = df.where(col("device_name").contains("device-mac")).groupBy(col("cn")).count.sort(desc("count")).limit(5)
+    println("Answer 2.3: 5 countries with the largest number of MAC devices ")
+    print(df2.show)
+
+    val df3 = df.groupBy(col("cn")).agg(avg(col("temp")).alias("avg_temp")).orderBy(desc("avg_temp")).limit(10)
+    println("Answer 2.4: 10 countries with the higest average temperature ")
+    print(df3.show(truncate=false))
+
+    spark.stop()
   }
 }
